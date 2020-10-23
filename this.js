12d38cf2870edff1bdae611c9b60459d4555b0b5
Phong Luu <luu.huynh@fiisoft.com>
2020-10-23 08:55:59 +0700

    this init

---
+++ this.js
@@ -0,0 +1,22 @@
+/*
+  `this` when use in a function, always refer to
+  global object.
+
+  `this` when use in a method (a kind of property hold a value function)
+  bind to the object who call it (method)
+*/
+
+let rabbitObj = {
+  speak: function() {
+    console.log(this)
+    // -> this refer to the caller object
+
+    function letSayThis(){
+      console.log(this)
+      // -> this refer to global object
+    }
+    letSayThis();
+  }
+}
+
+rabbitObj.speak();
