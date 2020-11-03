a51c380fb2bf5b24f6d3827605b52412a838cf90
Phong Luu <huynhpluu@gmail.com>
2020-11-03 20:54:02 +0700

    added: constructor function

--- constructor.js
+++ constructor.js
@@ -1,6 +1,5 @@
 // Three ways to create an object derives from a prototype
 
-
 /*
   Way 1: Through class notation.
   Create a constructor function named Person.
@@ -9,11 +8,11 @@
   hold function.
 */
 class Person {
-  constructor(name){
-    this.name = name
+  constructor(name) {
+    this.name = name;
   }
-  speak(line){
-    console.log(`${this.name} says ${line}`)
+  speak(line) {
+    console.log(`${this.name} says ${line}`);
   }
 }
 
@@ -26,35 +25,38 @@ Person.prototype.legs = 2;
   Create a function to generate an object with prototype personProto
 */
 let personProto = {
-  speak: function(line){
-    console.log(`${this.name} says ${line}`)
-  }
-}
+  speak: function (line) {
+    console.log(`${this.name} says ${line}`);
+  },
+};
 
-function PersonFunction(name){
-  let personObj = Object.create(personProto)
-  personObj.name = name
+function PersonFunction(name) {
+  let personObj = Object.create(personProto);
+  personObj.name = name;
   return personObj;
 }
-
 
 /*
   Way 3: Using Function Constructor.
   We must use new opperator to bind `this`
   to newly created object.
 */
-function PersonConstructor(name){
+function PersonConstructor(name) {
   this.name = name;
+
+  // never do this, every new instance created, we created a function.
+  // this.speak = function () {
+  //   console.log(this.name);
+  // };
 }
-PersonConstructor.prototype.speak = function(line){
-  console.log(`${this.name} says ${line}`)
-}
+PersonConstructor.prototype.speak = function (line) {
+  console.log(`${this.name} says ${line}`);
+};
 
+let person1 = new Person("John");
+let person2 = PersonFunction("Jane");
+let person3 = new PersonConstructor("Anna");
 
-let person1 = new Person("John")
-let person2 = PersonFunction("Jane")
-let person3 = new PersonConstructor("Anna")
-
-console.log(Object.getPrototypeOf(person1) === Person.prototype) // -> true
-console.log(Object.getPrototypeOf(person2) === personProto) // -> true
-console.log(Object.getPrototypeOf(person3) === PersonConstructor.prototype) // -> true
+console.log(Object.getPrototypeOf(person1) === Person.prototype); // -> true
+console.log(Object.getPrototypeOf(person2) === personProto); // -> true
+console.log(Object.getPrototypeOf(person3) === PersonConstructor.prototype); // -> true
