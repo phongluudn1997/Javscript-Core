feca2c830ba38bb001286b7cb055dc81b2043af9
Phong Luu <huynhpluu@gmail.com>
2021-03-03 12:36:16 +0700

    added: this

--- this.js
+++ this.js
@@ -1,31 +1,30 @@
-// "use strict";
-
 /*
-  `this` when use in a function, always refer to
-  global object.
+  `this` when use in a function, only refer to
+  global object in sloppy mode, in strict mode the value is undefined. 
 
   `this` when use in a method (a kind of property hold a value function)
   bind to the object who call it (method)
+
 */
 
-let rabbitObj = {
-  speak: function () {
-    console.log(this);
-    // -> this refer to the caller object
+// let rabbitObj = {
+//   speak: function () {
+//     console.log(this);
+//     // -> this refer to the caller object
 
-    function letSayThis() {
-      console.log(this);
-      // -> this refer to global object
-    }
-    letSayThis();
+//     function letSayThis() {
+//       console.log(this);
+//       // -> this refer to global object
+//     }
+//     letSayThis();
 
-    const arrFunc = () => {
-      console.log(this);
-      // -> because this arrow function is called in speak function
-      // so that it also has this.
-    };
-    arrFunc();
-  },
-};
+//     const arrFunc = () => {
+//       console.log(this);
+//       // -> because this arrow function is called in speak function
+//       // so that it also has this.
+//     };
+//     arrFunc();
+//   },
+// };
 
-rabbitObj.speak();
+// rabbitObj.speak();
