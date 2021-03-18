b99b59837e620930f158344c3d5ea5a4eb80a820
Phong Luu <huynhpluu@gmail.com>
2021-03-18 23:27:44 +0700

    added: git push

--- hoisting.js
+++ hoisting.js
@@ -5,3 +5,12 @@
  * let, const               no          TDZ                             block
  * arrow, expression func                       depend on var, let, const
  */
+
+"use strict";
+function a() {
+  if (true) {
+    function b() {}
+  }
+  console.log(b); // b in sloppy mode and error in strict mode
+}
+a();
