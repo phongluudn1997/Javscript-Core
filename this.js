dfdd524749e47e2e8ee9e89e078fde2c3499f069
lphuynh <luu.phong.huynh@mgm-tp.com>
2025-02-09 23:49:30 +0700

    fixed: typo

--- this.js
+++ this.js
@@ -4,24 +4,23 @@
 
   `this` when use in a method (a kind of property hold a value function)
   bind to the object who call it (method)
-
 */
 
 /**
  * Method => this: Object that is calling the method
  * Simple function call => this: undefined (strict mode) or window object (sloppy mode)
- * Arrow funcion => this: this of surrounding function (lexical this)
+ * Arrow function => this: this of surrounding function (lexical this)
  * Event listener => this: DOM Element that the handler is attached to
  */
 
 let rabbitObj = {
   speak: function () {
     console.log(this);
-    // -> this refer to the caller object
+    // -> this refers to the caller object
 
     function letSayThis() {
       console.log(this);
-      // -> this refer to global object
+      // -> this refers to global object
     }
     letSayThis();
 
